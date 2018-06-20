5c64ff957932da7b7b4d7f3f2121bb5dd76c4e4b
nabe01 <40374962+nabe01@users.noreply.github.com>
2018-06-20 10:17:56 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,16 +3,13 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while(True):
-
-    # Capture frame-by-frame
+    
     ret, frame = cap.read()
 
-    # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == 27:
         break
-
-# When everything done, release the capture
+        
 cap.release()
 cv2.destroyAllWindows()
 
